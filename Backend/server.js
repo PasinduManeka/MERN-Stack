f67d8f132cbacd2d91d7ef2a87badff5a08b2123
PasinduManeka <manekaherat815@gmail.com>
2021-07-15 18:39:02 +0530

    create the mongodb connection

--- Backend/server.js
+++ Backend/server.js
@@ -2,10 +2,42 @@
 //Then we can call the function easily
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+//const bodyParser = require("body-parser")
 const cors = require("cors");
-const dotenv = require("dotenv");
+//const dotenv = require("dotenv");
 const app = express();
+require("dotenv").config();
 
+//create a variable to define the server port number
+/* 8070 means whwn run the application on local computer, 
+we define a port number to run the application in the PC.
+local pc becomes the server*/
+//process.env.PORT means assign a available port number.
+const PORT = process.env.PORT || 8070;
 
+app.use(cors());
+//to pass the key value, due to mongodb has the jason format
+app.use(express.json());
 
+//get the stored url 
+const URL = process.env.MONGODB_URL;
+
+//make the configurations
+ mongoose.connect(URL,{
+     useCreateIndex:true,
+     useNewUrlParser:true,
+     useUnifiedTopologyL:true,
+     useFindAndModify:true
+ });
+
+ //create the connection
+const conn = mongoose.connection;
+//open the connection
+mongoose.connection.once("open", ()=>{
+    console.log("Mongodb Connection success!!");
+})
+
+//lisen the por
+app.listen(PORT,()=>{
+    console.log(`Server is up and running on port number: ${PORT}`)
+})
