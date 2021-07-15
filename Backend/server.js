d784d1f05868518fc2a69f28bfc69da1a004d13a
PasinduManeka <manekaherat815@gmail.com>
2021-07-15 16:07:57 +0530

    import the dependancies and assign to a variable

---
+++ Backend/server.js
@@ -0,0 +1,11 @@
+//imporrt a package and assign to a variable.
+//Then we can call the function easily
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+
+
+
