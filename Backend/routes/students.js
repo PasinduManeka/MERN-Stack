9a522a51a16dbbbcc8d6bafffba5af0ef7f9b3c8
PasinduManeka <manekaherat815@gmail.com>
2021-07-16 20:40:31 +0530

    set delete function

--- Backend/routes/students.js
+++ Backend/routes/students.js
@@ -37,7 +37,33 @@ router.route("/").get((req,res)=>{
 })
 
 //Find and update the student
+//using async, we can stop crash the requests   
+router.route("/update/:id").put(async (req,res)=>{
+    //body
+    //fetch the id from url.
+    let stuid = req.params.id;
+    //B structure
+    const {name,age,gender} = req.body;
 
+    const updateStudent = {
+        name,
+        age,
+        gender
+    }
+
+    //check the id is available in the mongodb and update 
+    //without if condiyion 
+    const update = await Student.findByIdAndUpdate(stuid,updateStudent).then(()=>{
+        res.status(200).send({status:"Status user updated",user:update})
+    }).catch((err)=>{
+        console.log(err);
+        res.status(404).send({status:"Error with updated data.",error:err.message});
+    })
+
+    
+
+
+})
 
 //Delete
 
