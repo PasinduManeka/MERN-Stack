0d32d7155b599d9ee40763cf6e6d816deb3a480a
PasinduManeka <manekaherat815@gmail.com>
2021-07-18 14:46:35 +0530

    create the function approach

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,6 +1,7 @@
 //import logo from './logo.svg';
 import './App.css';
 import CounterClass from './Components/counterclass';
+import CounterFunction from './Components/counterFunction';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <h1>Hello World</h1>
       <CounterClass/>
+      <CounterFunction/>
     </div>
   );
 }
---
+++ Frontend/src/Components/counterFunction.js
@@ -0,0 +1,18 @@
+import React, {useState} from "react";
+function CounterFunction(){
+    let [count, setCount] = useState(0);
+
+    function increment(){
+         setCount(++count);
+    }
+
+    return(
+        <div>
+            <h3>Functiobal Component</h3>
+            <h1>You have clicked {count} times.</h1>
+            <button className="btn btn-outline-danger" onClick={e=>increment()}>Increased the Count</button>
+        </div>
+    )
+}
+
+export default CounterFunction;
