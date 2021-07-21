3eaf434528b92481088c41a7e1f6a39a4910f5e7
PasinduManeka <manekaherat815@gmail.com>
2021-07-22 00:06:41 +0530

    Changd the post function in the routes

--- Backend/routes/students.js
+++ Backend/routes/students.js
@@ -2,35 +2,11 @@
 Route() function which is in the express*/ 
 const router = require("express").Router();
 
-//Import the student model file
-let Student = require("../models/student");
+const saveStudent = require("../Controller/student.controller");
 
 //post(Insert)
 //url component
-router.route("/add").post((req,res)=>{
-    //body
-    //creaate variables to assign the requested url body values
-    const name = req.body.name;
-    const age = Number(req.body.age);
-    const gender = req.body.gender;
-
-    const newStudent = new Student({
-        name,
-        age,
-        gender
-    })
-    
-    newStudent.save().then(()=>{
-        res.json("Student added.");
-    }).catch((err)=>{
-        console.log(err);
-        res.status(404).send({status:"Student has not been inserted."})
-
-    })
-    
-    
-    
-})
+router.post("/add",saveStudent)
 
 //Get(display)
 router.route("/").get((req,res)=>{
